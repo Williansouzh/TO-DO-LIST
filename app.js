585f0084d8532a98d43eb592aca646eb60b55806
Willian Souza <91621589+Williansouzh@users.noreply.github.com>
2022-06-29 21:25:20 -0300

    With help
    This is the version that i did with help.

--- app.js
+++ app.js
@@ -1,36 +1,70 @@
 'use strict';
-let toDoList = document.querySelector('.todo__list')
-let dataBase = []
-const taskText = document.querySelector('#textContent')
-const createTask = ()=>{
-    let div = document.createElement('label');
-    div.innerHTML = `
-        <label class="todo__item">
-        <input type="checkbox">
-        <div>${taskText.value}</div>
-        <input class="deleteTask" type="button" value="X">
-        </label>`;
-    toDoList.appendChild(div).classList.add('todo__item')
-}
-document.querySelector('.todo__new-item').addEventListener('keypress', (e)=>{
-    if(e.which==13){
-        createTask()
-    }
-}, false)
 
-const removeTask = (event)=>{
+
+const getDataBase = ()=> JSON.parse(localStorage.getItem('todoList')) ?? [] ;
+const setDataBase = (dataBase)=> localStorage.setItem('todoList', JSON.stringify(dataBase));
+
+
+const createitem = (text, stats, indice)=>{
+    const item = document.createElement('label');
+    item.classList.add('todo__item');
+    item.innerHTML = `
+        <input type="checkbox" ${stats} data-indice = ${indice}>
+        <div>${text}</div>
+        <input class="deleteTask" type="button" value="X" data-indice = ${indice}>
+    `;
+    document.getElementById('todoList').appendChild(item)
+}
+const cleanTasks = ()=>{
+    const todoList = document.getElementById('todoList');
+    while(todoList.firstChild){
+        todoList.removeChild(todoList.lastChild)
+    }
+}
+const updateList = ()=>{
+    cleanTasks()
+    const dataBase = getDataBase();
+    dataBase.forEach((item, indice)=> createitem(item.task, item.stats, indice));
+}
+
+const insertItem = (event)=>{
+    const keyPress = event.key;
+    const text = event.target.value;
+    console.log(keyPress)
+    if(keyPress==='Enter'){
+        const dataBase = getDataBase()
+        dataBase.push({'task': text, 'stats': ''});
+        setDataBase(dataBase) 
+        updateList(dataBase)
+        event.target.value = ''
+    }
+}
+const removeItem = (indice)=>{
+    const dataBase = getDataBase()
+    dataBase.splice(indice, 1);
+    setDataBase(dataBase);
+    updateList()
+}
+const updateItem= (indice)=>{
+    const dataBase = getDataBase()
+    dataBase[indice].stats =  dataBase[indice].stats ==='' ? 'checked' : '';
+    setDataBase(dataBase);
+    updateList()
+}
+const clickitem = (event)=>{
     const element = event.target;
-    if(element.type=='button'){
-
+    if(element.type==='button'){
+        const indice = element.dataset.indice;
+        removeItem(indice)
+    }else if(element.type === 'checkbox'){
+        const indice = element.dataset.indice;
+        updateItem(indice);
     }
 }
-const atualizarLista = ()=>document.querySelectorAll('.todo__item').forEach((task)=>{
-    dataBase.push(task)
-    task.addEventListener('click', removeTask)
-})
+document.getElementById('newItem').addEventListener('keypress', insertItem)
+document.getElementById('todoList').addEventListener('click', clickitem);
 
-atualizarLista()
-
+updateList()
 
 
 
